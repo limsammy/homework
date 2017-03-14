88d5185029f308d0d86b85bcfa26de33267a934f
Sam Lim <limsammy1@gmail.com>
2017-03-14 09:53:35 -0600

    rmed pseudocode comments

--- bad_connection.rb
+++ bad_connection.rb
@@ -1,17 +1,3 @@
-########Bad Connection Psuedocode##########
-
-# Greet user
-# prompt user for input
-# check to see if user entered anything
-# respond with "HELLO?!" if user didn't enter anything
-# if user entered something, check to see if there are any lowercase letters
-# respond "I AM HAVING A HARD TIME HEARING YOU." if there are lowercase letters
-
-# check if all uppercase
-# respond "NO, THIS IS NOT A PET STORE" if user inputted all uppercase letters
-# respond "ANYTHING ELSE I CAN HELP WITH" the first time user inputs "GOODBYE!"
-# respond "THANK YOU FOR CALLING" on second "GOODBYE!" input and exit program
-
 def run
 	goodbye_count = 0
 	
