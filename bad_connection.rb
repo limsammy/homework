665efeabcba933c5ca89c4301f7dd895a8775696
Sam Lim <limsammy1@gmail.com>
2017-03-16 10:55:56 -0600

    swapped for loop for until loop, removed paranthesis from conditionals

--- bad_connection.rb
+++ bad_connection.rb
@@ -1,25 +1,26 @@
 def run
 	goodbye_count = 0
+	exit = false
 	
 	puts "HELLO, THIS IS A GROCERY STORE!"
 
-	loop do
+	until exit == true do
 		print ">"
 		input = gets.chomp.to_s
-		if(input.length == 0)
+		if input.length == 0
 			puts "HELLO?!"
-		elsif(input != input.upcase)
+		elsif input != input.upcase
 			puts "I AM HAVING A HARD TIME HEARING YOU."
-		elsif(input == input.upcase && input != "GOODBYE!")
+		elsif input == input.upcase && input != "GOODBYE!"
 			puts "NO, THIS IS NOT A PET STORE"
 		end
 		
-		if(input == "GOODBYE!" && goodbye_count == 0)
+		if input == "GOODBYE!" && goodbye_count == 0
 			goodbye_count += 1
 			puts "ANYTHING ELSE I CAN HELP WITH"
-		elsif(goodbye_count == 1)
+		elsif goodbye_count == 1
 			puts "THANK YOU FOR CALLING"
-			break
+			exit = true
 		end
 	end
 end
