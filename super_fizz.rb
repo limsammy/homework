90e9fee37f3b982f7efc464ff6e52fb280a7c5c0
Sam Lim <limsammy1@gmail.com>
2017-03-17 11:28:31 -0600

    final version

--- super_fizz.rb
+++ super_fizz.rb
@@ -1,36 +1,37 @@
 class FizzBuilder
-def run
-	(0..1000).each do |num|
-		if(num % 3 == 0 && num % 5 == 0 && num % 7 == 0)
-			puts "SuperFizzBuzz"
-		elsif(num % 3 == 0 && num % 7 == 0)
-			puts "SuperFizz"
-		elsif(num % 5 == 0 && num % 7 == 0)
-			puts "SuperBuzz"
-		elsif(num % 3 == 0 && num % 5 == 0)
-			puts "FizzBuzz"
-		elsif(num % 3 == 0)
-			puts "Fizz"
-		elsif(num % 5 == 0)
-			puts "Buzz"
-		elsif(num % 7 == 0)
-			puts "Super"
-		else
-			puts num
+	def run
+		(0..1000).each do |num|
+			if(num % 3 == 0 && num % 5 == 0 && num % 7 == 0)
+				puts "SuperFizzBuzz"
+			elsif(num % 3 == 0 && num % 7 == 0)
+				puts "SuperFizz"
+			elsif(num % 5 == 0 && num % 7 == 0)
+				puts "SuperBuzz"
+			elsif(num % 3 == 0 && num % 5 == 0)
+				puts "FizzBuzz"
+			elsif(num % 3 == 0)
+				puts "Fizz"
+			elsif(num % 5 == 0)
+				puts "Buzz"
+			elsif(num % 7 == 0)
+				puts "Super"
+			else
+				puts num
+			end
 		end
+	end
+
+	def run_v2
+		(1..1000).each {|num|
+			msg = ''
+			msg += 'Super' if(num % 7 == 0)
+			msg += 'Fizz' if(num % 3 == 0)
+			msg += 'Buzz' if(num % 5 == 0)
+			puts (msg.empty? ? num : msg);
+		}
 	end
 end
 
-def run_v2
-	(1..1000).each {|num|
-	  msg = ''
-	  msg += 'Super' if(num % 7 == 0)
-	  msg += 'Fizz' if(num % 3 == 0)
-	  msg += 'Buzz' if(num % 5 == 0)
-	  puts (msg.empty? ? num : msg);
-	}
-end
-end
-
-run() # Time elapsed 3.3019999999999996 milliseconds
-run_v2() # Time elapsed 5.065 milliseconds
+superfizz = FizzBuilder.new
+superfizz.run # Time elapsed 3.3019999999999996 milliseconds
+superfizz.run_v2 # Time elapsed 5.065 milliseconds
