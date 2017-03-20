8aa8a6f5484f5ef50a7d41ed40e20e24d0c6d7f9
Sam Lim <limsammy1@gmail.com>
2017-03-20 14:38:32 -0600

    all tests pass

---
+++ calc/lib/calc.rb
@@ -0,0 +1,25 @@
+class Calculator
+  attr_reader :total
+
+  def initialize(total=0)
+    @total = total
+  end
+
+  def exist?
+    true
+  end
+
+  def add(num)
+    @total += num
+    num
+  end
+
+  def subtract(num)
+    @total -= num
+    num
+  end
+
+  def clear
+    @total = 0
+  end
+end
---
+++ calc/test/calc_test.rb
@@ -0,0 +1,38 @@
+gem 'minitest', '~> 5.0'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/calc'
+
+class CalculatorTest < Minitest::Test
+  def test_it_exists
+    calc = Calculator.new
+    assert calc.exist?
+  end
+
+  def test_add_1
+    calc = Calculator.new
+    assert_equal 1, calc.add(1)
+  end
+
+  def test_returns_total
+    calc = Calculator.new
+    calc.add(1)
+    calc.add(1)
+    assert_equal 2, calc.total
+  end
+
+  def test_subtract_1
+    calc = Calculator.new
+    calc.add(1)
+    calc.add(1)
+    calc.subtract(1)
+    assert_equal 1, calc.total
+  end
+
+  def test_clear
+    calc = Calculator.new
+    calc.add(25)
+    calc.clear
+    assert_equal 0, calc.total
+  end
+end
